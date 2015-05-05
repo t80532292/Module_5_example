ff506a8e667ad3c545027d00f48c85556a16af30
James Donaldson <kinakuta@Jamess-MacBook-Air.local>
2015-05-05 15:05:09 -0700

    added books data import script and placeholder static files

---
+++ booksJSON.js
@@ -0,0 +1,72 @@
+db.books.insert([
+	{
+		_id: 0,
+		title: 'Wool',
+		author: 'Hugh Howey',
+		genre: 'Science Fiction',
+		read: false
+	},
+	{
+		_id: 1,
+		title: 'To Kill a Mockingbird',
+		author: 'Harper Lee',
+		genre: 'Fiction',
+		read: false
+	},
+	{
+		_id: 2,
+		title: 'Dune',
+		author: 'Frank Herbert',
+		genre: 'Science Fiction',
+		read: false
+	},
+	{
+		_id: 3,
+		title: 'The Fall of the Ottomans',
+		author: 'Eugene Rogan',
+		genre: 'History',
+		read: false
+	},
+	{
+		_id: 4,
+		title: 'Empire Falls',
+		author: 'Richard Russo',
+		genre: 'Fiction',
+		read: false
+	},
+	{
+		_id: 5,
+		title: 'Shift',
+		author: 'Hugh Howey',
+		genre: 'Science Fiction',
+		read: false
+	},
+	{
+		_id: 6,
+		title: 'Truman',
+		author: 'David McCullough',
+		genre: 'History',
+		read: false
+	},
+	{
+		_id: 7,
+		title: 'The Amazing Adventures of Kavalier and Klay',
+		author: 'Michael Chabon',
+		genre: 'Fiction',
+		read: false
+	},
+	{
+		_id: 8,
+		title: 'Ulysses',
+		author: 'James Joyce',
+		genre: 'Fiction',
+		read: false
+	},
+	{
+		_id: 9,
+		title: 'The Hobbit',
+		author: 'J.R. Tolkien',
+		genre: 'Fantasy',
+		read: false
+	}
+])
